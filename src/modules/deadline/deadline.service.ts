241009d97f06ef0d9e4cd0028d60e4f6466db593
Laurcons <laurcons@outlook.com>
2023-06-12 21:19:19 +0300

    add gender

--- src/modules/deadline/deadline.service.ts
+++ src/modules/deadline/deadline.service.ts
@@ -3,8 +3,8 @@ import dayjs from "dayjs";
 
 @Injectable()
 export default class DeadlineService {
-  // deadline = dayjs('2023-06-25T00:00:00+03:00');
-  deadline = dayjs('2022-01-01');
+  deadline = dayjs('2023-06-25T23:59:00+03:00');
+  // deadline = dayjs('2022-01-01');
   constructor() {}
 
   get() {
